e4beacb4a64ddaa6eae6c9428f8598bb0f552d08
hrizeq17 <hrizeq@memphis.edu>
2024-02-05 21:39:16 -0600

    Add hints for too high or too low guesses

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,24 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+while True:
+    guess = input("What number am I thinking of? (Enter 'q' to quit): ")
+
+    if guess.lower() == 'q':
+        print(f"The correct number was {number}.")
+        break
+
+    try:
+        guess = int(guess)
+    except ValueError:
+        print("Invalid input. Please enter a number or 'q'.")
+        continue
+
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    elif guess < number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
